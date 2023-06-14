c83bb447955934be446f9b58d161bd1bf36ce9e1
MeshkovDI_PC <rdm.dima@mail.ru>
2023-06-15 02:03:38 +0700

    Send logs to Telegram to admin user

--- tg_bot_script.py
+++ tg_bot_script.py
@@ -1,3 +1,4 @@
+import logging
 import os
 from time import sleep
 
@@ -6,9 +7,20 @@ import telegram
 from dotenv import load_dotenv
 
 
+class TelegramLogsHandler(logging.Handler):
+
+    def __init__(self, tg_bot, chat_id):
+        super().__init__()
+        self.chat_id = chat_id
+        self.tg_bot = tg_bot
+
+    def emit(self, record):
+        log_entry = self.format(record)
+        self.tg_bot.send_message(chat_id=self.chat_id, text=log_entry)
+
+
 def main():
     load_dotenv()
-
     DEVMAN_AUTH_TOKEN = os.getenv('DEVMAN_AUTH_TOKEN')
     TG_BOT_TOKEN = os.getenv('TG_BOT_TOKEN')
     TG_CHAT_ID = os.getenv('TG_CHAT_ID')
@@ -16,7 +28,13 @@ def main():
     url = 'https://dvmn.org/api/long_polling'
     headers = {'Authorization': DEVMAN_AUTH_TOKEN}
 
-    bot = telegram.Bot(token=TG_BOT_TOKEN)
+    tg_bot = telegram.Bot(token=TG_BOT_TOKEN)
+
+
+    logger = logging.getLogger('Logger')
+    logger.setLevel(logging.INFO)
+    logger.addHandler(TelegramLogsHandler(tg_bot, TG_CHAT_ID))
+    logger.info('Бот запущен')
 
     payload = None
 
@@ -38,16 +56,16 @@ def main():
 
                 message = f'''Работа "{lesson_title}" проверена \n{checking_result} \nСсылка на урок: {lesson_url}'''
 
-                bot.send_message(chat_id=TG_CHAT_ID, text=message)
+                tg_bot.send_message(chat_id=TG_CHAT_ID, text=message)
 
         except requests.exceptions.ConnectTimeout as error:
-            print('ConnectTimeout')
+            logger.critical(f'Ошибка: \n{error}')
 
         except requests.exceptions.ReadTimeout as error:
-            print('ReadTimeout')
+            logger.critical(f'Ошибка: \n{error}')
 
-        except requests.exceptions.ConnectionError as e:
-            print(f'Error connecting to server: {e}')
+        except requests.exceptions.ConnectionError as error:
+            logger.critical(f'Ошибка: \n{error}')
             sleep(30)
 
 
